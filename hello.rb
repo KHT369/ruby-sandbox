83170e57cdc9381ba7983f27a35a5a4f1196487e
KHT369 <ktsao@chicagobooth.edu>
2024-10-19 14:44:34 +0000

    Version 1

---
+++ hello.rb
@@ -0,0 +1,34 @@
+require "active_support/all"
+require "awesome_print"
+require "pry-byebug"
+require "date"
+pp "hello, world!"
+
+#require "./goodbye.rb"
+
+require "date"
+
+x = Date.today
+
+pp rand(31).ordinalize
+
+
+pp x.day
+
+pp x.day.ordinalize
+
+# October 19th
+# October 20th
+# October 1st
+# October 3rd
+
+x = {
+  :apple => "banana",
+  "zebra" => [1 , 4, 6, 7]
+}
+pp x
+ap x
+
+11
+6
+29
